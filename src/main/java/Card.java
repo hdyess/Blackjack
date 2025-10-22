8e633c9843a6c6e15916426da4098cd11546a70a
hdyess <henrypdyess@gmail.com>
2025-10-22 16:02:29 -0400

    cleaning up copy and paste from workbook

---
+++ src/main/java/Card.java
@@ -0,0 +1,58 @@
+
+
+public class Card {
+
+	private String suit;
+	private String value;
+	private boolean isFaceUp;
+
+	public Card(String suit, String value) {
+		this.suit = suit;
+		this.value = value;
+		this.isFaceUp = false;
+	}
+
+	public String getSuit(){
+		// only return the suit if the card is face up
+		if(isFaceUp){
+			return suit;
+		} else {
+			return "#";
+		}
+	}
+
+	public String getValue(){
+		// only return the value if the card is face up
+		if(isFaceUp){
+			// this is the string value of the card
+			// i.e. A, K, Q, J, 10, 9 ...
+			return value;
+		}
+		return "#";
+	}
+
+	public int getPointValue(){
+		// only return the value if the card is face up
+		if(!isFaceUp){
+			return 0;
+		}
+		// determine point value and return it
+		// A = 11
+		// K, Q, J = 10
+		// all numeric cards are equal to their face value
+		try {
+			return Integer.parseInt(this.value);
+		} catch(Exception ex) {
+			return (value.equalsIgnoreCase("A")) ? 11 : 10;
+		}
+	}
+
+	public boolean isFaceUp(){
+		return isFaceUp;
+	}
+
+	public void flip(){
+		isFaceUp = !isFaceUp;
+	}
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Card gamer = new Card("Spades", "gamer");
+
+		gamer.flip();
+
+		gamer.getPointValue();
+		System.out.println(gamer.getPointValue());
+
+
+	}
+
+
+}
